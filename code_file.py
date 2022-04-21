da3aa7b86042eeb61517658483b77a057f3698bf
stphnredd <85525770+stphnredd@users.noreply.github.com>
2022-04-21 18:40:43 -0400

    Removed specific special characters
    Removed certain special characters

--- code_file.py
+++ code_file.py
@@ -2,7 +2,7 @@ import random
 
 print('Welcome to your Password Generator')
 
-chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXXYZ~`!@#$%^&*()_-+={[}]|\:;"<,>.?/0123456789'
+chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXXYZ~`!@#$%^&*+=:;?0123456789'
 
 number = input('Amount of passwords to generate: ')
 number = int(number)
